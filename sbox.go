ccbdfd9d06a748a0c4172d5f8e368658320e4a93
Edi Permadi <edipermadi@gmail.com>
2026-04-01 06:38:11 +0400

    refactor

--- sbox.go
+++ sbox.go
@@ -1,11 +1,7 @@
 package ascon
 
 func BasicSBOX(in uint) uint {
-	x0 := ((in >> 4) & 1) > 0
-	x1 := ((in >> 3) & 1) > 0
-	x2 := ((in >> 2) & 1) > 0
-	x3 := ((in >> 1) & 1) > 0
-	x4 := ((in >> 0) & 1) > 0
+	x0, x1, x2, x3, x4 := unpack(in)
 
 	y0 := xor(and(x4, x1), x3, and(x2, x1), x2, and(x1, x0), x1, x0)
 	y1 := xor(x4, and(x3, x2), and(x3, x1), x3, and(x2, x1), x2, x1, x0)
@@ -13,31 +9,11 @@ func BasicSBOX(in uint) uint {
 	y3 := xor(and(x4, x0), x4, and(x3, x0), x3, x2, x1, x0)
 	y4 := xor(and(x4, x1), x4, x3, and(x1, x0), x1)
 
-	out := uint(0)
-	if y0 {
-		out += 16
-	}
-	if y1 {
-		out += 8
-	}
-	if y2 {
-		out += 4
-	}
-	if y3 {
-		out += 2
-	}
-	if y4 {
-		out += 1
-	}
-	return out
+	return pack(y0, y1, y2, y3, y4)
 }
 
 func FactoredSBOX(in uint) uint {
-	x0 := ((in >> 4) & 1) > 0
-	x1 := ((in >> 3) & 1) > 0
-	x2 := ((in >> 2) & 1) > 0
-	x3 := ((in >> 1) & 1) > 0
-	x4 := ((in >> 0) & 1) > 0
+	x0, x1, x2, x3, x4 := unpack(in)
 
 	a0 := xor(x4, x0)
 	a1 := xor(x2, x1)
@@ -61,6 +37,19 @@ func FactoredSBOX(in uint) uint {
 	y3 := xor(c3, c2)
 	y4 := c4
 
+	return pack(y0, y1, y2, y3, y4)
+}
+
+func unpack(in uint) (bool, bool, bool, bool, bool) {
+	x0 := ((in >> 4) & 1) > 0
+	x1 := ((in >> 3) & 1) > 0
+	x2 := ((in >> 2) & 1) > 0
+	x3 := ((in >> 1) & 1) > 0
+	x4 := ((in >> 0) & 1) > 0
+	return x0, x1, x2, x3, x4
+}
+
+func pack(y0, y1, y2, y3, y4 bool) uint {
 	out := uint(0)
 	if y0 {
 		out += 16
